fe631dc32e0cf11d5c57efd61764b5c9cc9e798c
smashifique <33799153+smashifique@users.noreply.github.com>
2017-12-05 22:04:02 -0700

    Final
    final

--- js/index.js
+++ js/index.js
@@ -5,5 +5,19 @@ $(document).ready(function(){
     "font-family":"Verdana"
       });
   $("#para1").addClass("text-center");
+  $("#para2").css({
+    "color":"white",
+    "font-size":"15pt",
+    "font-family":"Verdana",
+    
+  });
+  $("#para2").addClass("text-center");
+  
+  $("#para3").css({
+    "color":"white",
+    "font-size":"11pt",
+    "font-family":"Verdana",
+    "text-decoration":"bold",
+  });
   
 });
