0a9d2f11ccc733dab3989ff6cefdf32f90e9d192
smashifique <33799153+smashifique@users.noreply.github.com>
2017-12-05 19:35:49 -0700

    Update new site
    Basketball John

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){  
+  $("#para1").css({
+    "color":"white",
+    "font-size":"12pt",
+    "font-family":"Verdana"
+      });
+  $("#para1").addClass("text-center");
+  
+});
